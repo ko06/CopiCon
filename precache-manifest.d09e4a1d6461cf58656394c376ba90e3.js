9c973c004d2d5b819f082340875d47067402a1ac
Selvaganapathi Kannan <selvaganapathi@mallow-tech.com>
2020-06-22 20:52:34 +0530

    Updates

---
+++ precache-manifest.d09e4a1d6461cf58656394c376ba90e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0431b2f3256c9ba2d9d19d1abe7ccdd5",
+    "url": "/CopiCon/index.html"
+  },
+  {
+    "revision": "0794dbbf6aacc740cbfc",
+    "url": "/CopiCon/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "5c15a30dd7b3ce05265e",
+    "url": "/CopiCon/static/css/main.25fcba2c.chunk.css"
+  },
+  {
+    "revision": "0794dbbf6aacc740cbfc",
+    "url": "/CopiCon/static/js/2.1e86685b.chunk.js"
+  },
+  {
+    "revision": "0ff6e886819e5acfe0088244d0a30a2c",
+    "url": "/CopiCon/static/js/2.1e86685b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c15a30dd7b3ce05265e",
+    "url": "/CopiCon/static/js/main.61e2149b.chunk.js"
+  },
+  {
+    "revision": "b304f9c88f74db41a571",
+    "url": "/CopiCon/static/js/runtime-main.afa470d3.js"
+  }
+]);
